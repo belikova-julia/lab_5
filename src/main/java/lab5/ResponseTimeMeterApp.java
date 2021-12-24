c0547e1b937505c17f9b1837e8f191f1cc29561a
belikova-julia <alistami38@gmail.com>
2021-12-24 20:49:08 +0300

    Scripted auto-commit on change (2021-12-24 20:49:08) by gitwatch.sh

--- src/main/java/lab5/ResponseTimeMeterApp.java
+++ src/main/java/lab5/ResponseTimeMeterApp.java
@@ -1,5 +1,18 @@
 package lab5;
 
+import akka.NotUsed;
+import akka.actor.ActorSystem;
+import akka.http.javadsl.ConnectHttp;
+import akka.http.javadsl.Http;
+import akka.http.javadsl.ServerBinding;
+import akka.http.javadsl.model.HttpRequest;
+import akka.http.javadsl.model.HttpResponse;
+import akka.stream.ActorMaterializer;
+import akka.stream.javadsl.Flow;
+
+import java.io.IOException;
+import java.util.concurrent.CompletionStage;
+
 public class ResponseTimeMeterApp {
     public static void main(String[] args) throws IOException {
         System.out.println("start!");
@@ -14,12 +27,10 @@ public class ResponseTimeMeterApp {
                 ConnectHttp.toHost("localhost", 8080),
                 materializer
         );
-        System.out.println("Server online at http://localhost:8080/\nPress
-                RETURN to stop...");
+        System.out.println("Server online at http://localhost:8080/\n");
         System.in.read();
         binding
                 .thenCompose(ServerBinding::unbind)
-                .thenAccept(unbound -> system.terminate()); // and shutdown
-        when done
+                .thenAccept(unbound -> system.terminate());
     }
 }
