39845819292bc62d32744ad1fd4a8b7132d95579
belikova-julia <alistami38@gmail.com>
2021-12-24 21:57:51 +0300

    Scripted auto-commit on change (2021-12-24 21:57:51) by gitwatch.sh

--- src/main/java/lab5/StoreMessage.java
+++ src/main/java/lab5/StoreMessage.java
@@ -1,4 +1,19 @@
 package lab5;
 
 public class StoreMessage {
+    private final String url;
+    private final float avgTime;
+
+    public StoreMessage(String url, float avgTime) {
+        this.url = url;
+        this.avgTime = avgTime;
+    }
+
+    public float getAvgTime() {
+        return avgTime;
+    }
+
+    public String getUrl() {
+        return url;
+    }
 }
